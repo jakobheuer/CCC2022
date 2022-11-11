9240cf03347c77793cbcd67319df370b1d6c93e3
jakobheuer <jakob.heigl@me.com>
2022-11-11 14:21:02 +0100

    init

--- lvl1/src/com/heiglauer/LvlOneMain.java
+++ lvl1/src/com/heiglauer/LvlOneMain.java
@@ -1,2 +1,21 @@
-package com.heiglauer;public class LvlOneMain {
+package com.heiglauer;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class LvlOneMain {
+    public static void main(String[] args) {
+
+        String file ="src/test/resources/fileTest.txt";
+        try(BufferedReader reader = new BufferedReader(new FileReader(file))){
+            String currentLine = reader.readLine();
+            reader.close();
+        }
+        catch (IOException e) {
+            System.out.println("File Error");
+            e.printStackTrace();
+        }
+    }
 }
